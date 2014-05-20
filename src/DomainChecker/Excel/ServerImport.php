6b317c51a27ab52ee6f427bb2253add2e850d99d
W4cko <W4cko@users.noreply.github.com>
2014-05-20 17:29:48 +0200

    Update ServerImport.php

--- src/DomainChecker/Excel/ServerImport.php
+++ src/DomainChecker/Excel/ServerImport.php
@@ -5,28 +5,28 @@ namespace DomainChecker\Excel;
 use DomainChecker\Database\Database;
 
 class ServerImport extends AImport
-{	
-	/**
-	* Return Servers datas
-	* Refer the sheetname
-	*/
-	public function getServersDatas()
-	{
-		$this->loadSheet('serveurs');
-		$this->formatDatas();
-		return $this->datas;
-	}
+{    
+    /**
+    * Return Servers datas
+    * Refer the sheetname
+    */
+    public function getServersDatas()
+    {
+        $this->loadSheet('serveurs');
+        $this->formatDatas();
+        return $this->datas;
+    }
 
-	/**
-	* Add and save servers in Database
-	*
-	*/
-	public function saveServer($database)
-	{
-		$servers = $this->getServersDatas();
-		foreach ($servers as $ip => $value) {
-			$database->addServer($ip,$value);
-		}
-		$database->save();
-	}
+    /**
+    * Add and save servers in Database
+    *
+    */
+    public function saveServer($database)
+    {
+        $servers = $this->getServersDatas();
+        foreach ($servers as $ip => $value) {
+            $database->addServer($ip,$value);
+        }
+        $database->save();
+    }
 }
